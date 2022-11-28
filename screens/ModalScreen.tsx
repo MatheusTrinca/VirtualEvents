638d39afdb7c5d513dcd62cf5a9d4cb067c1b1fa
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-11-28 19:49:20 -0300

    Event detail modal

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,15 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import { Image, Platform, StyleSheet } from 'react-native';
 import { Text, View } from '../components/Themed';
+import event from '../assets/data/event.json';
+import { AntDesign } from '@expo/vector-icons';
+import CustomButton from '../components/CustomButton';
+import users from '../assets/data/users.json';
+import { RootStackScreenProps } from '../types';
 
-export default function ModalScreen() {
+export default function ModalScreen({ route }: RootStackScreenProps<'Modal'>) {
+  const id = route?.params?.id;
+
+  const onJoin = () => {};
+
+  const displayedUsers = users.slice(0, 5);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/ModalScreen.tsx" />
+      <Text style={styles.title}>{event.name}</Text>
+      <Text style={styles.time}>
+        <AntDesign name="calendar" size={24} color={'black'} />{' '}
+        {new Date(event.date).toDateString()}
+      </Text>
+
+      <View style={styles.footer}>
+        {/* Users avatars */}
+        <View style={styles.user}>
+          {displayedUsers.map((user, index) => (
+            <Image
+              key={user.id}
+              source={{ uri: user.avatarUrl }}
+              style={[
+                styles.userAvatar,
+                { transform: [{ translateX: -15 * index }] },
+              ]}
+            />
+          ))}
+          <View
+            style={[
+              styles.usersAvatar,
+              { transform: [{ translateX: -15 * displayedUsers.length }] },
+            ]}
+          >
+            <Text>+{users.length - displayedUsers.length}</Text>
+          </View>
+        </View>
+        <CustomButton text="Join the event" onPress={onJoin} />
+      </View>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
@@ -20,16 +56,39 @@ export default function ModalScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    paddingBottom: 25,
   },
   title: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
+    marginVertical: 10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  time: {
+    fontSize: 20,
+  },
+  footer: {
+    marginTop: 'auto',
+  },
+
+  userAvatar: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    margin: 2,
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+  user: {
+    flexDirection: 'row',
+  },
+  usersAvatar: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    margin: 2,
+    backgroundColor: 'gainsboro',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,42 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import { Alert, Pressable, StyleSheet } from 'react-native';
+import { Agenda, AgendaEntry } from 'react-native-calendars';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import events from '../assets/data/events.json';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<'TabOne'>) {
+  const renderEmptyDate = () => {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>This is empty date!</Text>
+      </View>
+    );
+  };
+
+  const renderItem = (reservation: AgendaEntry, isFirst: boolean) => {
+    const fontSize = isFirst ? 16 : 14;
+    const color = isFirst ? 'black' : '#43515c';
+
+    return (
+      <Pressable
+        style={[styles.item, { height: reservation.height }]}
+        onPress={() => navigation.navigate('Modal', { id: reservation.id })}
+      >
+        <Text style={{ fontSize, color }}>{reservation.name}</Text>
+      </Pressable>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Agenda
+        items={events}
+        renderItem={renderItem}
+        selected={'2022-11-25'}
+        renderEmptyDate={renderEmptyDate}
+      />
     </View>
   );
 }
@@ -17,16 +44,19 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30,
+    paddingLeft: 10,
   },
 });
